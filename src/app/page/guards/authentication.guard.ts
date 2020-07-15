51d059aacb99feeb97a5129763ccf81157e8ba17
Alvaro <0alvarorm0@gmail.com>
2020-07-15 20:22:24 +0100

    Guards añadidos

---
+++ src/app/page/guards/authentication.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthenticateService } from '../services/authentication.service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationGuard implements CanActivate {
+  constructor(private authSvc: AuthenticateService, private router: Router) {
+  }
+
+  canActivate(
+      next: ActivatedRouteSnapshot,
+      state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authSvc.isLogged) {
+      return true;
+    } else {
+      console.log('Acceso denegado');
+      this.router.navigateByUrl('/login').then((e) => {
+        if (e) {
+          console.log('Navigation is successful!');
+        } else {
+          console.log('Navigation has failed!');
+        }
+        return false;
+      });
+    }
+  }
+}
