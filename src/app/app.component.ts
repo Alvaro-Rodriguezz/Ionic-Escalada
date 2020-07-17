af4549071e2fe32889e7992de9a7a9e74ff1d5a3
Alvaro <0alvarorm0@gmail.com>
2020-07-17 19:37:25 +0100

    Splash añadido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import {timer} from 'rxjs';
+import {Platform} from '@ionic/angular';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  showSplash = true;
 
+  constructor(private platform: Platform,
+              private statusBar: StatusBar,
+              private splashScreen: SplashScreen){
+    this.initializeApp();
+  }
+
+  initializeApp(){
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      timer(3000).subscribe(() => this.showSplash = false);
+    });
+  }
 }
